c2980f10a99af24389b9e40a1efb856b3b86b364
bistpramod <bistpramod113@gmail.com>
2026-03-28 00:21:02 +0545

    Update OrderForm component

--- src/components/OrderForm.tsx
+++ src/components/OrderForm.tsx
@@ -5,12 +5,40 @@ interface OrderFormProps {
 }
 
 function OrderForm({ onSubmit }: OrderFormProps) {
-    const [name, setName] = useState<string>("Pizza");
-    const [cups,setCups] = useState<number>(1);
-  return <form onSubmit= {handleSubmit}>
-    <label >Chai name</label>
-    <input value ={name} onChange= {[(e: React.ChangeEvent<HTMLInputElement>) => setName(e.target.value)]} />
-  </form>
+  const [name, setName] = useState<string>("Pizza");
+  const [cups, setCups] = useState<number>(1);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    onSubmit({
+      name,
+      cups,
+    });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>Chai name</label>
+      <input
+        value={name}
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+          setName(e.target.value)
+        }
+      />
+
+      <label>Cups</label>
+      <input
+        type="number"
+        value={cups}
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+          setCups(Number(e.target.value) || 0)
+        }
+      />
+
+      <button type="submit">Submit</button>
+    </form>
+  );
 }
 
 export default OrderForm;
