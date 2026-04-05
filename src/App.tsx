8d337878b113042bffa27d47da8cc6a095357807
Pra <155552124+bistpramod@users.noreply.github.com>
2026-04-05 23:48:58 +0545

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,30 @@
 import './App.css'
-import {Card} from "./components/Card";
-import { Counter } from './components/Counter';
-import type {Chai} from "./types";
-import { List } from './components/List';
+import { Card } from "./components/Card"
+import { Counter } from './components/Counter'
+import { List } from './components/List'
+import type { Chai } from "./types"
 
-
-const menu : Chai[]=[
-  {id:1 , name:"Pizza", price:300},
-  {id:2 , name:"Burger", price:200},
-  {id:3 , name:"Pasta", price:250},
+const menu: Chai[] = [
+  { id: 1, name: "Pizza",  price: 300 },
+  { id: 2, name: "Burger", price: 200 },
+  { id: 3, name: "Pasta",  price: 250 },
 ]
-function App() {
- 
 
+function App() {
   return (
     <>
       <div>
         <h1>Vite + React</h1>
-        <Card name ="pizza" price={500} />
-        <Card name ="Key" price={50} />
+        <Card name="pizza" price={500} />
+        <Card name="Key"   price={50}  />
       </div>
+
       <div>
-        <Counter/>
+        <Counter />
       </div>
+
       <div>
-        <List items= {menu}/>
+        <List items={menu} />
       </div>
     </>
   )
